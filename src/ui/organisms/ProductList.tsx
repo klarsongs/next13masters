82ef04255d5c1198ec13ce5b11444995ad450214
Klara Jędrzejewska <klarsongs@gmail.com>
2023-09-07 11:57:46 +0200

    Move products list test id

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -3,7 +3,10 @@ import { type Product } from "@/ui/types";
 
 export const ProductList = ({ products }: { products: Product[] }) => {
 	return (
-		<ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid md:grid-cols-3 lg:grid lg:grid-cols-4">
+		<ul
+			className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid md:grid-cols-3 lg:grid lg:grid-cols-4"
+			data-testid="products-list"
+		>
 			{products.map((product) => (
 				<ProductListItem key={product.id} product={product} />
 			))}
